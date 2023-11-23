602f1c8b525cfbc46acf7755cafbaad4ea1a9c26
FedorenkoTaras <tarakhanio84@gmail.com>
2023-11-23 23:43:22 +0200

    Initial commit

---
+++ src/main/java/com/geeksforless/tfedorenko/DoctorsGuideApplication.java
@@ -0,0 +1,11 @@
+package com.geeksforless.tfedorenko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctorsGuideApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DoctorsGuideApplication.class,args);
+    }
+}
